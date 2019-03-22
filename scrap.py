d9652ae1b07341f599d0d46e71622d7b15f0b87c
Sen Le <senvanle4@gmail.com>
2019-03-23 01:47:03 +1100

    scrap

--- scrap.py
+++ scrap.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
+import pprint
 import json
 
 
